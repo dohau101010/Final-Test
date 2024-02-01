6d2ca24e45480cbe52702b242277e6f6497045e8
DoTrungHau <dotrunghau101023.st@gmail.com>
2024-02-01 17:52:31 +0700

    IoTs Project

--- js/scripts1.js
+++ js/scripts1.js
@@ -6,40 +6,39 @@ var btnOn3 = document.getElementById("btnon_03");
 var btnOff3 = document.getElementById("btnoff_03");
 
 btnOn1.onclick = function () {
-  document.getElementById("led_01").src = "/img/ledon.jpg";
+  document.getElementById("led_01").src = "../img/ledon.jpg";
   firebase.database().ref("/Led").update({
     Led: 1,
   });
 };
 
 btnOff1.onclick = function () {
-  document.getElementById("led_01").src = "/img/ledoff.jpg";
+  document.getElementById("led_01").src = "../img/ledoff.jpg";
   firebase.database().ref("/Led").update({
     Led: 0,
   });
 };
 btnOn2.onclick = function () {
-  document.getElementById("fan_01").src = "/img/fanon.jpg";
+  document.getElementById("fan_01").src = "../img/fanon.jpg";
   firebase.database().ref("/Fan").update({
     Fan: 1,
   });
 };
 
 btnOff2.onclick = function () {
-  document.getElementById("fan_01").src = "/img/fanoff.jpg";
+  document.getElementById("fan_01").src = "../img/fanoff.jpg";
   firebase.database().ref("/Fan").update({
     Fan: 0,
   });
 };
 btnOn3.onclick = function () {
-  document.getElementById("air_01").src = "/img/air-on.jpg";
+  document.getElementById("air_01").src = "../img/air-on.jpg";
   firebase.database().ref("/Air").update({
     Air: 1,
   });
 };
-
 btnOff3.onclick = function () {
-  document.getElementById("air_01").src = "/img/air-off.jpg";
+  document.getElementById("air_01").src = "../img/air-off.jpg";
   firebase.database().ref("/Air").update({
     Air: 0,
   });
